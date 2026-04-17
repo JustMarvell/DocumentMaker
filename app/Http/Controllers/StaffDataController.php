540537da3f0bdee4c8b6d6bbb3092710a36b0a0c
JustMarvell <marvelmakaluwu050405@gmail.com>
2026-04-17 08:46:14 +0800

    fix bug

--- app/Http/Controllers/StaffDataController.php
+++ app/Http/Controllers/StaffDataController.php
@@ -8,6 +8,10 @@ use App\Models\StaffData;
 class StaffDataController extends Controller
 {
 
+    public function index() {
+        return response()->json(StaffData::orderBy('staff_name')->get());
+    }
+
     public function store(Request $request) {
         // return as JSON derulo.....
         $request->validate([
@@ -15,17 +19,22 @@ class StaffDataController extends Controller
             'nip' => 'required|string|unique:staff_data,nip',
             'email' => 'required|email|unique:staff_data,email',
             'work_unit' => 'required|string|max:255',
-            'rank' => 'required|string|max:255',
-            'position' => 'required|string|max:255',
-            'phone_number' => 'required|string|max:20',
+            'rank' => 'nullable|string|max:255',
+            'position' => 'nullable|string|max:255',
+            'phone_number' => 'nullable|string|max:20',
         ]);
-    
+
         StaffData::create($request->only([
-            'staff_name', 'nip', 'email',
-            'phone_number', 'rank', 'position', 'work_unit',
+            'staff_name',
+            'nip',
+            'email',
+            'phone_number',
+            'rank',
+            'position',
+            'work_unit',
         ]));
-    
-        return back()->with('success', "Data Staff {$request->staff_name} berhasil ditambahkan");
+
+        return back()->with('success', "Data staff {$request->staff_name} berhasil ditambahkan.");
     }
 
     public function update(Request $request , StaffData $staffDatum) {
