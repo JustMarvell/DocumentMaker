8c7ec6c5e76ce126a1f599dc23dae61670b2e0b9
JustMarvell <marvelmakaluwu050405@gmail.com>
2026-04-21 08:11:54 +0800

    force https sceme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +12,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Source - https://stackoverflow.com/a/75049850
+        // Posted by Murtaza Noori, modified by community. See post 'Timeline' for change history
+        // Retrieved 2026-04-21, License - CC BY-SA 4.0
+
+        if (env(key: 'APP_ENV') !== 'local') {
+            URL::forceScheme(scheme: 'https');
+        }
+
     }
 
     /**
