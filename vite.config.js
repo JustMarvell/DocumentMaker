39822fdd8b1ed590f2ac977ea1e756381a1c16ca
JustMarvell <marvelmakaluwu050405@gmail.com>
2026-04-23 10:38:07 +0800

    fix problem with ngrok cant use localhost

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,15 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                // for future ref.
+                // add path here if use additional css or js
+                // just if use ngrok
+                // but also if use other like ngrok
+
+                'resources/css/app.css',
+                'resources/css/home.css',
+                'resources/js/app.js',],
             refresh: true,
         }),
     ],
