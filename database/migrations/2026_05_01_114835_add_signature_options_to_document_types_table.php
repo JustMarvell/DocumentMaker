c02ff06766b9ac3042e18337e4f167d8246b6e4f
JustMarvell <marvelmakaluwu050405@gmail.com>
2026-05-01 19:51:29 +0800

    add use qr and image to the migration table

---
+++ database/migrations/2026_05_01_114835_add_signature_options_to_document_types_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('document_types', function (Blueprint $table) {
+            $table->boolean('signature_use_image')->default(true)->after('signature_enabled');
+            $table->boolean('signature_use_qr')->default(true)->after('signature_use_image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('document_types', function (Blueprint $table) {
+            //
+        });
+    }
+};
