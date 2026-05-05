8dfae9fdbc46de695353ff57a3ac70427416c6ad
JustMarvell <marvelmakaluwu050405@gmail.com>
2026-05-06 03:20:30 +0800

    add relationship

--- app/Models/DocumentLog.php
+++ app/Models/DocumentLog.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use App\Models\SignatureRequest;
 
 class DocumentLog extends Model
 {
@@ -26,4 +28,5 @@ class DocumentLog extends Model
     public function user() : BelongsTo { return $this->belongsTo(User::class); }
     public function documentType(): BelongsTo { return $this->belongsTo(DocumentType::class); }
     public function status(): BelongsTo { return $this->belongsTo(DocumentLog::class); }
+    public function signatureRequest(): HasOne { return $this->hasOne(SignatureRequest::class); }
 }
